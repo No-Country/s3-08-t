dd1528c2fc73e77905b4fe6ea36773614f4d42aa
storres20 <sanchotorres777@gmail.com>
2022-08-12 01:11:27 -0500

    ADD TextInput buscar

--- src/screens/PacienteScreen.js
+++ src/screens/PacienteScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, Image, StyleSheet, ScrollView } from 'react-native'
+import { View, Text, TouchableOpacity, Image, StyleSheet, ScrollView, TextInput } from 'react-native'
 import React, { useState } from 'react'
 
 import { Formik } from 'formik';
@@ -27,17 +27,30 @@ export const PacienteScreen = () => {
   return (
     <ScrollView >
       <View style={styles.container}>
-        <Image style={styles.logo} source={require('../../assets/doctor.png')} />
+        <Image style={styles.logoDoctor} source={require('../../assets/doctor.png')} />
         <View>
           <Text style={styles.text1}>Buenas Noches,</Text>
           <Text style={styles.text2}>Maria das Flores!</Text>
         </View>
       </View>
       
-      <View>
-        <Text>Mi Historial</Text>
-        <CustomInput />
+      <View style={styles.container2}>
+        <Text style={styles.text3}>Mi Historial</Text>
+        
+        <View style={styles.container3}>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={{flex:1, fontSize: 20, fontFamily: 'robotoLight', color: '#fff'}}
+              placeholder="buscar"
+              placeholderTextColor='#fff'
+              selectionColor={'#40B590'}
+            />
+            <Image source={require('../../assets/buscar.png')} style={styles.ImageStyle} />
+          </View>
+        </View>
+        
       </View>
+      
       
     </ScrollView>
   )
@@ -53,7 +66,7 @@ const styles = StyleSheet.create({
     marginLeft: 21,
     marginRight: 12
   },
-  logo: {
+  logoDoctor: {
     width: 77,
     height: 102,
     marginTop: 0,
@@ -69,5 +82,45 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontFamily: 'robotoMedium',
     textAlign: 'right'
-  }
+  },
+  
+  container2: {
+    paddingHorizontal:21,
+  },
+  text3: {
+    fontSize: 20,
+    fontFamily: 'robotoMedium',
+    marginVertical: 12,
+  },
+  
+  
+  container3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    //margin: 10,
+  },
+  
+  SectionStyle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(64, 181, 144, 0.5)',
+    //borderWidth: .5,
+    //borderColor: '#000',
+    height: 80,
+    borderRadius: 18 ,
+    //margin: 10,
+    
+    paddingLeft: 27,
+  },
+   
+  ImageStyle: {
+    padding: 10,
+    margin: 13,
+    height: 56,
+    width: 56,
+    resizeMode : 'stretch',
+    alignItems: 'center'
+  },
 })
