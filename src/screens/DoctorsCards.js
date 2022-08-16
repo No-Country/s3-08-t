b19c582abec59a3562a76d0c53d155a1a88f2a12
cesargaleano <87844938+cesargaleano@users.noreply.github.com>
2022-08-16 17:57:40 -0500

    Fix Conflicts in Components
    HeaderPatient
    DoctorsCards

--- src/screens/DoctorsCards.js
+++ src/screens/DoctorsCards.js
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
 import { View, Text, ScrollView, SafeAreaView, StyleSheet } from 'react-native'
-=======
-import { View, Text, ScrollView, SafeAreaView,StyleSheet } from 'react-native'
->>>>>>> d5d0434fbf67d22fcbc8ff1c0cd7a462d3519d59
 import React from 'react';
 import { DoctorCard } from '../components/DoctorCard';
 import { HeaderPatient } from '../components/HeaderPatient';
 import Greting from '../components/Greting'
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 24,
-      backgroundColor: "#F2F2F2",
-     
-    },
-})
 
 
 
@@ -29,7 +17,6 @@ export const DoctorsCards = () => {
 
         <SafeAreaView>
 
-<<<<<<< HEAD
             <ScrollView>
 
                 <HeaderPatient name='Ezequiel Sanchez' />
@@ -41,11 +28,6 @@ export const DoctorsCards = () => {
                     <DoctorCard name='Cesar Galeano Torres ' especialidad='Cardiologo' />
                     <DoctorCard name='Jaime Agudelo' especialidad='Neumonologo' />
                     <DoctorCard name='Italo' especialidad='Traumatologo' />
-=======
-            <ScrollView >
-                <View style={styles.container}>
-                    <Greting name='Ezequiel Sanchez'/>
->>>>>>> d5d0434fbf67d22fcbc8ff1c0cd7a462d3519d59
                 </View>
                     <View style={{ width: 300,justifyContent: 'center', alignItems: 'center', marginTop: 10, marginLeft:10 }}>
                         <Text style={{ fontSize: 20, textAlign: 'center',  fontWeight:"500" }}>Marcar Citas {'>'} Especialidad {'>'} Doctor</Text>
