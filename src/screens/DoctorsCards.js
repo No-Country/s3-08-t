930faf902bea12bfa0a15ec296b5f9be5e363778
cesargaleano <87844938+cesargaleano@users.noreply.github.com>
2022-08-30 05:40:28 -0500

    Flow App

--- src/screens/DoctorsCards.js
+++ src/screens/DoctorsCards.js
@@ -1,35 +1,19 @@
 
 import { View, Text, ScrollView, SafeAreaView, StyleSheet } from 'react-native'
-
-
-
 import React, {useEffect} from 'react';
 import { DoctorCard } from '../components/DoctorCard';
-
-
 import Greting from '../components/Greting'
 import Footer from '../components/Footer';
-
+import { useSelector } from 'react-redux';
 
 
 
 
 
 export const DoctorsCards = () => {
+    const doctors = useSelector(state=>state.doctor.doctors);
+    const {dni, name, img="https://media-exp1.licdn.com/dms/image/D4D35AQEce8GvFyg66A/profile-framedphoto-shrink_200_200/0/1650208419921?e=1662300000&v=beta&t=eYMKeC0kowgXgQhJfT7KZQ4q6dizfjvGw5SoZULH2Zs"} = useSelector(state=>state.user.sesionInfo.pat)
 
-    const [doctors, setDoctors] = React.useState([])
-    const doctorsEndpont = 'https://app-healtconnect.herokuapp.com/api/doctor/';
-
-    useEffect(() => {
-        fetch(doctorsEndpont)
-            .then(response => response.json())
-            .then(response => {
-                setDoctors([...response.userDoctor])
-                console.log(response.userDoctor)
-            })
-    }, [])
-
-    console.log(doctors)
 
     return (
 
@@ -37,15 +21,17 @@ export const DoctorsCards = () => {
         <SafeAreaView>
             <ScrollView >
                 <View style={styles.container}>
-                    <Greting name='Ezequiel Sanchez' />
+                <Greting name= {name} />
+      
                 </View>
                 <View style={styles.titleCtn}>
                     <Text style={styles.title}>Marcar Citas {'>'} Especialidad {'>'} Doctor</Text>
+                    
                 </View>
                 <View style={styles.cardsContainer}>
-                    
-                    {doctors.map(doctors =>
-                        <DoctorCard key={doctors._id} id={doctors._id} name={doctors.doctorName} especialidad={doctors.type} source={{uri:doctors.user[0].img}} />
+                  
+                    {doctors.map(doctor =>
+                        <DoctorCard key={doctor._id} id={doctor._id} name={doctor.doctorName} especialidad={doctor.type} source={{uri:doctor.user[0].img}} />
                     )
 
                     }
@@ -77,9 +63,10 @@ const styles = StyleSheet.create({
     cardsContainer: {
         width: 300,
         justifyContent: 'center',
-        alignItems: 'center',
+        
         width: 300,
-        marginLeft: 10
+        marginLeft: 10,
+        
     },
     container: {
         flex: 1,
